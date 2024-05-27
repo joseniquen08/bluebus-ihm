24518c8e7085a5acdad252937a502d4dad3d7662
XatoPo <flaviovm2013@gmail.com>
2024-05-27 12:31:03 -0500

    CAMBIO DE LOGIN A INDEX Y EL INDEX ANTERIOR A INICIO

--- index.php
+++ index.php
@@ -6,238 +6,83 @@ include_once './Conexion.php';
 $obj = new Agencia();
 // Define una variable para almacenar el mensaje de error
 $mensaje_error = '';
+
+// Verifica si se han enviado datos del formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtén los valores del formulario
+    $usuario_correo = $_POST['email'];
+    $contraseña = $_POST['password'];
+
+    // Llama a tu función validarLogin
+    if ($obj->validarLogin($usuario_correo, $contraseña)) {
+        // Si las credenciales son válidas, redirige al usuario a la página de inicio
+        header("Location: inicio.php");
+        exit();
+    } else {
+        // Si las credenciales no son válidas, muestra un mensaje de error
+        $mensaje_error = "Credenciales incorrectas. Intente de nuevo.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio - Empresa de Transporte Terrestre</title>
-    <!-- Enlace a la hoja de estilos de Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Enlace a los iconos de Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="css/incio_style.css" rel="stylesheet" type="text/css"/>
-  </head>
 
-  <body>
-    <!-- Barra de Navegación -->
-    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src="images/BlueBus_logo.png" alt="BlueBus Logo" class="logo mb-4" height="35" width="auto">
-        </a>
-        <!-- Botón de hamburguesa para dispositivos móviles -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Elementos de navegación -->
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php"><i class="bi bi-house-door"></i> Inicio</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><i class="bi bi-info-circle"></i> Nosotros</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><i class="bi bi-people"></i> Servicios</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#"><i class="bi bi-envelope"></i> Contacto</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>BlueBus - Login</title>
+  <!-- Enlace a la hoja de estilos de Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Enlace a los iconos de Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link href="css/style-login.css" rel="stylesheet" type="text/css"/>
+</head>
 
-  <!-- Slider de Viajes -->
-  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="images/pexels-mauricio-espinoza-gavilano-582278929-17029842.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Lima</h5>
-            <p>Disfruta de nuestras excursiones a las mejores playas del país.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-            <img src="images/pexels-sbenavides-17043614.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Arequipa</h5>
-            <p>Descubre la belleza natural de nuestro país con nuestros tours ecológicos.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-            <img src="images/pexels-cristianloayza-16198315.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Trujillo</h5>
-            <p>Descubre la belleza natural de nuestro país con nuestros tours ecológicos.</p>
-          </div>
-        </div>
-        <!-- Agrega más elementos carousel-item según sea necesario -->
-      </div>
-      <!-- Flechas de control -->
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+<body class="d-flex justify-content-center align-items-center vh-100 bg-color-blue-oscuro">
+  <div class="bg-white rounded-5 text-secondary d-flex justify-content-between" style="width: 60rem">
+    <div class="left-box shadow">
+        <img src="images/pexels-mauricio-espinoza-gavilano-582278929-17029844.jpg" alt="Machu Picchu" style="border-top-left-radius: 2rem; border-bottom-left-radius: 2rem;">
     </div>
-    
-    <br></br>
-
-    <!-- Contenido Principal -->
-    <div class="container py-5">
-      <h1 class="display-3 mb-5 text-center">Encuentra tu destino ideal</h1>
-      <form action="search.php" method="GET" class="search-form">
-        <div class="row justify-content-center">
-          <div class="col-md-4">
-            <div class="input-group mb-3">
-              <label class="input-group-text" for="inputGroupSelect01">Origen</label>
-              <select class="form-select" id="inputGroupSelect01">
-              <option selected>Elije...</option>
-                <option value="1">LIMA</option>
-                <option value="2">ICA</option>
-                <option value="3">AREQUIPA</option>
-                <option value="4">CAMANA</option>
-                <option value="5">CHIMBOTE</option>
-                <option value="5">TRUJILLO</option>
-                <option value="5">HUARMEY</option>
-                <option value="5">CHICLAYO</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="input-group mb-3">
-              <label class="input-group-text" for="inputGroupSelect02">Destino</label>
-              <select class="form-select" id="inputGroupSelect02">
-                <option selected>Elije...</option>
-                <option value="1">LIMA</option>
-                <option value="2">ICA</option>
-                <option value="3">AREQUIPA</option>
-                <option value="4">CAMANA</option>
-                <option value="5">CHIMBOTE</option>
-                <option value="5">TRUJILLO</option>
-                <option value="5">HUARMEY</option>
-                <option value="5">CHICLAYO</option>
-              </select>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="input-group mb-3">
-              <label class="input-group-text" for="inputGroupSelect03">Fecha de Salida</label>
-              <input type="date" class="form-control" id="inputGroupSelect03" required>
-            </div>
-          </div>
-          <div class="col-md-1">
-          <button type="submit" class="btn btn-primary" formaction="seleccionar_asiento.php?cod_via='VIA0001'">Buscar</button>
-          </div>
+    <div class="right-box">
+      <div class="p-5 text-secondary shadow" style="width: 25rem; border-top-right-radius: 2rem; border-bottom-right-radius: 2rem;">
+        <div class="d-flex justify-content-center">
+          <img src="images/login-icon.svg" alt="login-icon" style="height: 7rem" />
         </div>
-      </form>
-    </div>
-
-    <!-- Contenedores de Viajes -->
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active"> <!-- Añadir la clase 'active' al primer item -->
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-4">
-                <div class="card mb-4">
-                    <img src="images/ballenas-tumbes-turismo.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Lima - Tumbes</h5>
-                    <p class="card-text">Explora los mejores destinos costeros con nosotros.</p>
-                    <a href="selecAsiento.php" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
-                  </div>
-                </div>
-              </div>
-              <!-- Agregar más tarjetas aquí -->
-              <div class="col-lg-4">
-                <div class="card mb-4">
-                    <img src="images/pexels-sbenavides-17043614.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Lima - Aqrequipa</h5>
-                    <p class="card-text">Descripción corta del destino.</p>
-                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4">
-                <div class="card mb-4">
-                    <img src="images/pexels-mauricio-espinoza-gavilano-582278929-17029844.jpg" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Arequipa - Lima</h5>
-                    <p class="card-text">Descripción corta del destino.</p>
-                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
-                  </div>
-                </div>
-              </div>
+        <div class="text-center fs-1 fw-bold">Login</div>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST"> <!-- Aquí se especifica la acción y método del formulario -->
+          <div class="input-group mt-4">
+            <div class="input-group-text bg-color-blue-oscuro">
+              <img src="images/username-icon.svg" alt="username-icon" style="height: 1rem" />
             </div>
+            <input name="email" class="form-control bg-light" type="text" placeholder="Correo electrónico" />
           </div>
+          <div class="input-group mt-1">
+            <div class="input-group-text bg-color-blue-oscuro">
+              <img src="images/password-icon.svg" alt="password-icon" style="height: 1rem" />
+            </div>
+            <input name="password" class="form-control bg-light" type="password" placeholder="Contraseña" />
+          </div>
+          <button type="submit" class="btn btn-blue-oscuro text-white w-100 mt-4 fw-semibold shadow-sm">Iniciar sesión</button>
+        </form>
+        <?php if(!empty($mensaje_error)) { ?>
+            <div class="alert alert-danger mt-3" role="alert">
+                <?php echo $mensaje_error; ?>
+            </div>
+        <?php } ?>
+        <div class="d-flex gap-1 justify-content-center mt-1">
+          <div>¿No estás registrado?</div>
+          <a href="#" class="text-decoration-none text-blue-oscuro fw-semibold">Registrate</a>
         </div>
-        <div class="carousel-item">
-          <div class="container">
-            <div class="row">
-              <!-- Aquí comienza un nuevo carousel-item -->
-              <div class="col-lg-4">
-                <div class="card mb-4">
-                  <img src="https://via.placeholder.com/300" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Destino 4</h5>
-                    <p class="card-text">Descripción corta del destino.</p>
-                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4">
-                <div class="card mb-4">
-                  <img src="https://via.placeholder.com/300" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Destino 5</h5>
-                    <p class="card-text">Descripción corta del destino.</p>
-                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4">
-                <div class="card mb-4">
-                  <img src="https://via.placeholder.com/300" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Destino 6</h5>
-                    <p class="card-text">Descripción corta del destino.</p>
-                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <!-- Resto del contenido -->
+        <div class="d-flex justify-content-around mt-3">
+          <a href="#" class="text-decoration-none text-blue-oscuro">¿Olvidaste tu contraseña?</a>
+          <a href="#" class="text-decoration-none text-blue-oscuro">Contacto</a>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only"></span>
-      </a>
     </div>
+  </div>
+</body>
 
-    <!-- Pie de Página -->
-    <footer class="bg-info text-white text-center py-4">
-      <div class="container">
-        <p class="mb-0"><i class="bi bi-telephone"></i> Teléfono: +123 456 789 | <i class="bi bi-geo-alt"></i> Dirección: Av. Principal 123, Ciudad Principal, País</p>
-        <p class="mb-0"><i class="bi bi-envelope"></i> Email: info@empresa.com</p>
-      </div>
-    </footer>
-
-    <!-- Script de Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
 </html>
---
+++ inicio.php
@@ -0,0 +1,243 @@
+<?php
+// Incluye el archivo Agencia.php que contiene tu función validarLogin
+include_once './Agencia.php';
+include_once './Conexion.php';
+
+$obj = new Agencia();
+// Define una variable para almacenar el mensaje de error
+$mensaje_error = '';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - Empresa de Transporte Terrestre</title>
+    <!-- Enlace a la hoja de estilos de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Enlace a los iconos de Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="css/incio_style.css" rel="stylesheet" type="text/css"/>
+  </head>
+
+  <body>
+    <!-- Barra de Navegación -->
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="images/BlueBus_logo.png" alt="BlueBus Logo" class="logo mb-4" height="35" width="auto">
+        </a>
+        <!-- Botón de hamburguesa para dispositivos móviles -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Elementos de navegación -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php"><i class="bi bi-house-door"></i> Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="bi bi-info-circle"></i> Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="bi bi-people"></i> Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="bi bi-envelope"></i> Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  <!-- Slider de Viajes -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="images/pexels-mauricio-espinoza-gavilano-582278929-17029842.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Lima</h5>
+            <p>Disfruta de nuestras excursiones a las mejores playas del país.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+            <img src="images/pexels-sbenavides-17043614.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Arequipa</h5>
+            <p>Descubre la belleza natural de nuestro país con nuestros tours ecológicos.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+            <img src="images/pexels-cristianloayza-16198315.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Trujillo</h5>
+            <p>Descubre la belleza natural de nuestro país con nuestros tours ecológicos.</p>
+          </div>
+        </div>
+        <!-- Agrega más elementos carousel-item según sea necesario -->
+      </div>
+      <!-- Flechas de control -->
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    
+    <br></br>
+
+    <!-- Contenido Principal -->
+    <div class="container py-5">
+      <h1 class="display-3 mb-5 text-center">Encuentra tu destino ideal</h1>
+      <form action="search.php" method="GET" class="search-form">
+        <div class="row justify-content-center">
+          <div class="col-md-4">
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect01">Origen</label>
+              <select class="form-select" id="inputGroupSelect01">
+              <option selected>Elije...</option>
+                <option value="1">LIMA</option>
+                <option value="2">ICA</option>
+                <option value="3">AREQUIPA</option>
+                <option value="4">CAMANA</option>
+                <option value="5">CHIMBOTE</option>
+                <option value="5">TRUJILLO</option>
+                <option value="5">HUARMEY</option>
+                <option value="5">CHICLAYO</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect02">Destino</label>
+              <select class="form-select" id="inputGroupSelect02">
+                <option selected>Elije...</option>
+                <option value="1">LIMA</option>
+                <option value="2">ICA</option>
+                <option value="3">AREQUIPA</option>
+                <option value="4">CAMANA</option>
+                <option value="5">CHIMBOTE</option>
+                <option value="5">TRUJILLO</option>
+                <option value="5">HUARMEY</option>
+                <option value="5">CHICLAYO</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="input-group mb-3">
+              <label class="input-group-text" for="inputGroupSelect03">Fecha de Salida</label>
+              <input type="date" class="form-control" id="inputGroupSelect03" required>
+            </div>
+          </div>
+          <div class="col-md-1">
+          <button type="submit" class="btn btn-primary" formaction="seleccionar_asiento.php?cod_via='VIA0001'">Buscar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <!-- Contenedores de Viajes -->
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active"> <!-- Añadir la clase 'active' al primer item -->
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="card mb-4">
+                    <img src="images/ballenas-tumbes-turismo.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Lima - Tumbes</h5>
+                    <p class="card-text">Explora los mejores destinos costeros con nosotros.</p>
+                    <a href="selecAsiento.php" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
+                  </div>
+                </div>
+              </div>
+              <!-- Agregar más tarjetas aquí -->
+              <div class="col-lg-4">
+                <div class="card mb-4">
+                    <img src="images/pexels-sbenavides-17043614.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Lima - Aqrequipa</h5>
+                    <p class="card-text">Descripción corta del destino.</p>
+                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="card mb-4">
+                    <img src="images/pexels-mauricio-espinoza-gavilano-582278929-17029844.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Arequipa - Lima</h5>
+                    <p class="card-text">Descripción corta del destino.</p>
+                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="container">
+            <div class="row">
+              <!-- Aquí comienza un nuevo carousel-item -->
+              <div class="col-lg-4">
+                <div class="card mb-4">
+                  <img src="https://via.placeholder.com/300" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Destino 4</h5>
+                    <p class="card-text">Descripción corta del destino.</p>
+                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="card mb-4">
+                  <img src="https://via.placeholder.com/300" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Destino 5</h5>
+                    <p class="card-text">Descripción corta del destino.</p>
+                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="card mb-4">
+                  <img src="https://via.placeholder.com/300" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Destino 6</h5>
+                    <p class="card-text">Descripción corta del destino.</p>
+                    <a href="#" class="btn btn-primary"><i class="bi bi-eye"></i> Ver Detalles</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only"></span>
+      </a>
+    </div>
+
+    <!-- Pie de Página -->
+    <footer class="bg-info text-white text-center py-4">
+      <div class="container">
+        <p class="mb-0"><i class="bi bi-telephone"></i> Teléfono: +123 456 789 | <i class="bi bi-geo-alt"></i> Dirección: Av. Principal 123, Ciudad Principal, País</p>
+        <p class="mb-0"><i class="bi bi-envelope"></i> Email: info@empresa.com</p>
+      </div>
+    </footer>
+
+    <!-- Script de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
