562a62a4235080d443cd830d70b0cba3a2dd6abf
XatoPo <flaviovm2013@gmail.com>
2024-05-28 18:11:06 -0500

    VIAJES

--- viajes.php
+++ viajes.php
@@ -42,31 +42,29 @@ if (isset($_GET['origen'], $_GET['destino'], $_GET['fecha']) && $_GET['origen'] 
             </div>
             <div class="row justify-content-center" id="results-container">
                 <?php foreach ($viajes as $viaje): ?>
-                <div class="col-md-8 mb-4 viaje-card">
-                    data-price="<?php echo htmlspecialchars($viaje['PRECIO_BASE']); ?>"
-                    data-duration="<?php echo htmlspecialchars($viaje['DURACION']); ?>"
-                    data-earliest="<?php echo htmlspecialchars($viaje['HORA_VIA']); ?>"
+                <div class="col-md-8 mb-4 viaje-card" 
+                        data-price="<?php echo htmlspecialchars($viaje['PRECIO_BASE']); ?>"
+                        data-duration="<?php echo htmlspecialchars($viaje['DURACION']); ?>"
+                        data-earliest="<?php echo htmlspecialchars($viaje['HORA_VIA']); ?>">
                     <div class="card">
                         <div class="card-body">
                             <div class="row align-items-center">
                                 <div class="col-md-2 text-center">
                                     <h5 class="card-title"><?php echo htmlspecialchars($viaje['HORA_VIA']); ?> am</h5>
-                                    <p class="mb-0"><?php echo htmlspecialchars($viaje['ORIGEN']); ?></p>
-                                </div>
-                                <div class="col-md-1 text-center">
-                                    <i class="bi bi-arrow-right"></i>
-                                </div>
-                                <div class="col-md-2 text-center">
-                                    <p class="mb-0"><?php echo htmlspecialchars($viaje['DESTINO']); ?></p>
+                                    <p class="mb-0"><?php echo htmlspecialchars($origen_nombre); ?></p>
                                 </div>
                                 <div class="col-md-2 text-center">
                                     <p class="card-text mb-0"><?php echo htmlspecialchars($viaje['DURACION']); ?></p>
-                                </div>
-                                <div class="col-md-3 text-center">
-                                    <p class="card-text mb-0"><?php echo htmlspecialchars($viaje['FECHA_VIA']); ?></p>
+                                    <i class="bi bi-arrow-right"></i>
                                 </div>
                                 <div class="col-md-2 text-center">
-                                    <a href="#" class="btn btn-price">S/.<?php echo htmlspecialchars($viaje['PRECIO_BASE']); ?> <i class="bi bi-arrow-right"></i></a>
+                                    <p class="mb-0"><?php echo htmlspecialchars($destino_nombre); ?></p>
+                                </div>
+                                <div class="col-md-3 text-center">
+                                    <p class="card-text mb-0">Fecha: <?php echo htmlspecialchars($viaje['FECHA_VIA']); ?></p>
+                                </div>
+                                <div class="col-md-2 text-center">
+                                    <a href="seleccionar_asiento.php?COD_VIA=<?php echo $viaje['COD_VIA']; ?>" class="btn btn-price">S/.<?php echo htmlspecialchars($viaje['PRECIO_BASE']); ?> <i class="bi bi-arrow-right"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -107,9 +105,9 @@ if (isset($_GET['origen'], $_GET['destino'], $_GET['fecha']) && $_GET['origen'] 
                             valB = new Date('1970/01/01 ' + b.getAttribute('data-earliest'));
                             break;
                         case 'latest':
-                            valA = new Date('1970/01/01 ' + a.getAttribute('data-latest'));
-                            valB = new Date('1970/01/01 ' + b.getAttribute('data-latest'));
-                            break;
+                            valA = new Date('1970/01/01 ' + a.getAttribute('data-earliest'));
+                            valB = new Date('1970/01/01 ' + b.getAttribute('data-earliest'));
+                            return valB - valA; // Reverse order for latest
                     }
 
                     return valA - valB;
